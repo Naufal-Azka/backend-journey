9ae5645ba10ffa29432d9c60d591ea10672faada
Naufal Azka <askanaufal74@gmail.com>
2025-01-12 21:49:14 +0700

    commit

---
+++ learning-new-language/collections.py
@@ -0,0 +1,43 @@
+# kayak array
+
+
+# LIST 
+fruits = ["apple",'orange','banana','coconut']
+# fruits[0] = "pineapple"
+
+# print(dir(fruits))
+# print(help(fruits))
+# print("cherry" in fruits)
+
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pinapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+# fruits.index("apple")
+# fruits.count("apple")
+
+print(fruits)
+# for fruit in fruits:
+#     print(fruit)
+
+
+# SET
+fruits_set = {"apple",'orange','banana','coconut'}
+
+# fruits_set.add("pineapple")
+# fruits_set.remove("apple")
+# fruits_set.pop()
+# fruits_set.clear()
+
+print(fruits_set)
+
+
+# TUPLE
+fruits_tuple = ("apple",'orange','banana','coconut')
+
+fruits_tuple.count("apple")
+fruits.index("apple")
+
+print(fruits_tuple)
