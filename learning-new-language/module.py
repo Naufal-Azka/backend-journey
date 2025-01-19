9871a62484864eb182ed1cc2a41eb71403e134fe
Naufal Azka <askanaufal74@gmail.com>
2025-01-19 17:45:57 +0700

    Add mathematical functions for square, cube, circumference, and area calculations

---
+++ learning-new-language/module.py
@@ -0,0 +1,22 @@
+import math
+# import math as m
+# from math import e
+
+a, b, c, d, e= 1, 2, 3, 4, 5
+
+print(math.e ** a)
+print(math.e ** b)
+print(math.e ** c)
+print(math.e ** d)
+print(f'{math.e ** e}\n')
+
+
+# import module
+import module_example as module
+
+result = module.square(5)
+result2 = module.cube(5)
+result3 = module.circumference(5)
+result4 = module.area(5)
+
+print(result)
